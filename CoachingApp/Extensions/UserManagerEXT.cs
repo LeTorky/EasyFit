33ad127273e5e17bc2dd8c0164839ed28c486d4f
Torky <24torky@gmail.com>
2022-06-18 23:58:31 +0200

    -Added Custom UserManager

---
+++ CoachingApp/Extensions/UserManagerEXT.cs
@@ -0,0 +1,29 @@
+﻿using CoachingApp.Identity;
+using CoachingApp.Implementations;
+using CoachingApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace CoachingApp.Extensions
+{
+    public class UserManagerEXT : UserManager<IdentityApplicationUser>
+    {
+        private IdentityApplicationContext _context;
+        public UserManagerEXT(IUserStore<IdentityApplicationUser> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<IdentityApplicationUser> passwordHasher, IEnumerable<IUserValidator<IdentityApplicationUser>> userValidators, IEnumerable<IPasswordValidator<IdentityApplicationUser>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<IdentityApplicationUser>> logger, IdentityApplicationContext context)
+            : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+        {
+            _context = context;
+        }
+        public async Task<Coach> GetCoachAsync(ClaimsPrincipal User)
+        {
+            IdentityApplicationUser IdentityUser = await GetUserAsync(User);
+            return _context.Coaches.Where(Coach => Coach.User == IdentityUser).FirstOrDefault();
+        }
+        public async Task<Client> GetClientAsync(ClaimsPrincipal User)
+        {
+            IdentityApplicationUser IdentityUser = await GetUserAsync(User);
+            return _context.Clients.Where(Client => Client.User == IdentityUser).FirstOrDefault();
+        }
+    }
+}
--- CoachingApp/Program.cs
+++ CoachingApp/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using CoachingApp.Interfaces;
 using CoachingApp.Implementations;
+using CoachingApp.Extensions;
 // Create application builder.
 var builder = WebApplication.CreateBuilder(args); // Creates builder.
 
@@ -26,6 +27,7 @@ builder.Services.AddIdentity<IdentityApplicationUser, IdentityApplicationRoles>(
     SignInOpt.RequireConfirmedAccount = false;
     setupAction.SignIn = SignInOpt;
 }).AddEntityFrameworkStores<IdentityApplicationContext>() // Adds custom Identity DBContext.
+.AddUserManager<UserManagerEXT>() // Adds custom User Manager.
 .AddDefaultTokenProviders(); // Adds default Token Provider.
 builder.Services.AddAuthorization(); // Adds Authorization.
 builder.Services.AddCors(options => // Cross Origin Policy.
