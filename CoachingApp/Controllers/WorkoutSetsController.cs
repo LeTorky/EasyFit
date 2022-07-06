3411c072cc8e847adfac4c72218b01f49428bbb6
ahmed <ahmed_elsokkary98@yahoo.com>
2022-07-06 20:33:03 +0200

    edit workout sets getting coach method

--- CoachingApp/Controllers/WorkoutSetsController.cs
+++ CoachingApp/Controllers/WorkoutSetsController.cs
@@ -14,10 +14,10 @@ namespace CoachingApp.Controllers
     {
         private IWorkoutSetsManager _workoutSetsManager;
         private ICoachManager _coachManager;
-        private readonly SignInManager<IdentityApplicationUser> _signInManager;
+        private readonly IdentityUserManager _signInManager;
         private Coach Coach;
 
-        public WorkoutSetsController(IWorkoutSetsManager workoutSetsManager, ICoachManager coachManager, SignInManager<IdentityApplicationUser> signInManager)
+        public WorkoutSetsController(IWorkoutSetsManager workoutSetsManager, IdentityUserManager signInManager, ICoachManager coachManager)
         {
             _workoutSetsManager = workoutSetsManager;
             _coachManager = coachManager;   
@@ -28,7 +28,7 @@ namespace CoachingApp.Controllers
         [Authorize(Roles ="Coach")]
         public async Task<IActionResult> getWorkoutSetsByCoachID()
         {
-            Coach = (await _signInManager.UserManager.GetUserAsync(User)).Coach;
+            Coach = (await _signInManager.GetCoachAsync(User));
             if (!_coachManager.isCoach(Coach.id))
                 return NotFound("coach isnot registered!");
             return Ok(_workoutSetsManager.getWorkoutsByCoachID(Coach.id));
@@ -38,7 +38,7 @@ namespace CoachingApp.Controllers
         [Authorize(Roles = "Coach")]
         public async Task<IActionResult> addWorkoutSet([FromBody] WorkoutSet workoutSet)
         {
-            Coach = (await _signInManager.UserManager.GetUserAsync(User)).Coach;
+            Coach = (await _signInManager.GetCoachAsync(User));
 
             if (!_coachManager.isCoach(Coach.id))
                 return NotFound("coach isnot registered!");
@@ -54,7 +54,7 @@ namespace CoachingApp.Controllers
         [Authorize(Roles = "Coach")]
         public async Task<IActionResult> updateWorkoutSet(int setID, [FromBody] WorkoutSet workoutSet)
         {
-            Coach = (await _signInManager.UserManager.GetUserAsync(User)).Coach;
+            Coach = (await _signInManager.GetCoachAsync(User));
 
             if (!_coachManager.isCoach(workoutSet.coachID))
                 return NotFound("coach isnot registered!");
@@ -70,7 +70,7 @@ namespace CoachingApp.Controllers
         [Authorize(Roles = "Coach")]
         public async Task<IActionResult> deleteWorkoutSet(int setID)
         {
-            Coach = (await _signInManager.UserManager.GetUserAsync(User)).Coach;
+            Coach = (await _signInManager.GetCoachAsync(User));
 
             if (!_workoutSetsManager.setExists(setID))
                 return NotFound("workout set doesnot exist!");
