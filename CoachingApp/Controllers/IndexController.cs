72ec596b10dcc4afc5c121c3436ac8bd0ad2fb99
Torky <24torky@gmail.com>
2022-07-14 16:34:46 +0200

    Testing Same Origin Cookie

---
+++ CoachingApp/Controllers/IndexController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoachingApp.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet]
+        public ContentResult Index()
+        {
+            return new ContentResult()
+            {
+                Content ="<script>" + @"fetch('https://localhost:7109/api/Account/Login', { method:""POST"", credentials: 'include', headers:{ 'Content-Type': 'application/json', 'Accept': 'application/json', 'Access-Control-Allow-Origin' : 'https://coachingg.herokuapp.com', }, body: '{""userName"":""TestingOne"", ""password"":""aA1@3000""}' } ).then(x=>x.json().then(y=>console.log(y)))"+
+                "</script>",
+                ContentType = "text/html"
+            };
+        }
+    }
+}
+
